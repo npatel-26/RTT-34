b9aac0d6d1d47c915988461e63f337dc38b71a6d
npatel-26 <npatel.job@gmail.com>
2024-08-22 18:51:32 -0400

    add more code for math functions

--- basicmath.py
+++ basicmath.py
@@ -1,3 +1,19 @@
 print(1 + 1)
 print(100/33)
 print(2**2)
+
+
+easy_math = 1+1
+one_third = 100/33
+powerful = 2**2
+
+print(easy_math)
+print(one_third)
+print(powerful)
+
+
+x = 2 
+y = 4
+z = x+y    
+print(x + y)
+print (z)
