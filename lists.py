cc5f31ba4cb92d6ec7577fc042d58b5f10f00832
npatel-26 <npatel.job@gmail.com>
2024-08-22 21:05:52 -0400

    added for loops to print values

--- lists.py
+++ lists.py
@@ -1,6 +1,22 @@
 class_roster= ["Xiulan","Kwaku","Shirley"]
 test_scores= [86,93,80]
 mix_List = ["Xiulan", 99, "Kwaku", 54, 3.14159, "Shirley", 1.05]
-print(class_roster)
-print(test_scores)
-print(mix_List)
+print("class_roster list:  " + str(class_roster))
+print("test_scores list:  " + str(test_scores))
+print("mix_List list:  " + str(mix_List))
+
+print("\n")
+print("class_roster:")
+for student in class_roster:
+    print(student)
+
+print("\n")
+print("test_scores:")
+for score in test_scores:
+    print(score)
+
+print("\n")
+print("mix_List:")
+for num in mix_List:
+    print(num)
+
