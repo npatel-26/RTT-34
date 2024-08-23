a82ce8a95fce44346fbc0358fde7ce1eca137e41
npatel-26 <npatel.job@gmail.com>
2024-08-22 20:41:50 -0400

    Demo of conditional-ifs

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+#number of Bananas var
+numBananas = 0
+#If there are 5 or more bananas print “I have a bunch of bananas.”
+#if there are 1–4 bananas print “I have a small bunch of bananas.”
+#If there are no bananas print “I have no bananas.”
+if ( numBananas >= 5 ):
+    print("I have a bunch of bananas.")
+elif (numBananas >=1 | numBananas <= 4):
+    print("I have a small bunch of bananas.")
+else:
+    print("I have no bananas")
+
+
+
