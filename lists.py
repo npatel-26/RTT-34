6a492ceea60472c2b711204c88ba50a10c7135e0
npatel-26 <npatel.job@gmail.com>
2024-08-22 20:55:52 -0400

    Demo of list and mixed lists

---
+++ lists.py
@@ -0,0 +1,6 @@
+class_roster= ["Xiulan","Kwaku","Shirley"]
+test_scores= [86,93,80]
+mix_List = ["Xiulan", 99, "Kwaku", 54, 3.14159, "Shirley", 1.05]
+print(class_roster)
+print(test_scores)
+print(mix_List)
